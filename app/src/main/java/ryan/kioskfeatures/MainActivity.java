716c00229249784718edbd104f0fef655d6a17fd
RyanE <Ryan.E.Tibbetts@gmail.com>
2016-12-19 13:30:26 -0500

    initial commit

---
+++ app/src/main/java/ryan/kioskfeatures/MainActivity.java
@@ -0,0 +1,20 @@
+package ryan.kioskfeatures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // nothing to do here
+        // … really
+    }
+
+}
